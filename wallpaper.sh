23ae520e315cd418b90a6d6175fab8a3c7397485
basu-dev <adbasudev54@gmail.com>
2021-05-30 10:35:19 +0545

    wallpaper prev problem fixed

--- wallpaper.sh
+++ wallpaper.sh
@@ -9,7 +9,7 @@ setWallpaper(){
     echo "feh --no-fehbg --bg-fill '$1'" > ~/.fehbg
 }
 fromMenu(){
-    image=$(ls $walpaperFolder*.jpg | sxiv -tio)
+    image=$(ls $walpaperFolder*.jpg $walpaperFolder*.png | sxiv -tio)
     setWallpaper "$image"
     exit 0
 }
@@ -21,14 +21,14 @@ tmp=/tmp/wallpapers
 ls $walpaperFolder*.jpg  $walpaperFolder*.png > $tmp 
 totalPictures=$(wc -l < $tmp)
 readIndex(){
-index=$(tail -1 $config | awk '{print $NF}')
+    index=$(tail -1 $config | awk '{print $NF}')
     [ ${#index} == 0  ] && echo "wallpaperIndex = 1" > $config && readIndex
 }
 readIndex
-control='next'
-[ $control = 'next' ] && index=`expr $index + 1` || index=`expr $index - 1`
+[ $control == 'next' ] && index=`expr $index + 1` || index=`expr $index - 1`
 [ $index -gt $totalPictures ] && index=1 
 [ $index -lt 1 ] && index=`expr $totalPictures + 1`
+
 echo "wallpaperIndex = $index" > $config 
 image=$(head -n $index $tmp | tail -1 )
 setWallpaper "$image"
